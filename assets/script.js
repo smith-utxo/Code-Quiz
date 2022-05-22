63d026e2fbfd9640172a6c560396bd4ac588932c
Adam Smith <smith.adam937@gmail.com>
2022-05-21 22:47:18 -0500

    WIP: HTML outline & Question Card beginning

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var startButton = document.getElementById('start-btn');
+
+startButton = addEventListener('click', startQuiz);
+
+
+function startQuiz() {
+
+ // alert("you have begun the quiz!"); 
+}
