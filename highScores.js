9e6c0db5087f4d26f6e29739097132b21bc3eeff
Adam Smith <smith.adam937@gmail.com>
2022-05-27 14:57:42 -0500

    clear highscores btn/functionality added to highscores page

--- highScores.js
+++ highScores.js
@@ -1,6 +1,7 @@
 var highScoresList = document.querySelector("#actual-scores");
 const restartQuiz = document.getElementById('back-home'); 
 var mostRecentScore = localStorage.getItem('mostRecentScore');
+const clearScores = document.getElementById('clear-scores');
 
 // Grab the player initials and scores. If empty, just create an empty array
 var highScores = JSON.parse(localStorage.getItem('highScores')) || []; 
@@ -12,8 +13,11 @@ highScoresList.innerHTML = highScores.map(function(score) {
 })
 .join("");
 
+clearScores.addEventListener('click', function(){
+  localStorage.removeItem('highScores');
+  highScoresList.innerHTML = ""; 
 
-
+})
 
 restartQuiz.addEventListener('click', function(){
     location.replace("index.html"); 
